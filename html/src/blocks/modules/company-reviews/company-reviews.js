950c78eed13c9de36301e3074cef62daa3e59ffd
Ruslan Garapov <ruslangarapov1994@gmail.com>
2021-08-10 13:24:19 +0500

    company partners buttons created

---
+++ html/src/blocks/modules/company-reviews/company-reviews.js
@@ -0,0 +1,13 @@
+modules.define('company-reviews', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
