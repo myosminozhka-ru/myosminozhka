9fb34e289571fc9e22892c7c420c1d333a7ad8dc
Ruslan Garapov <ruslangarapov1994@gmail.com>
2021-08-12 21:09:05 +0500

    blog page created

---
+++ html/src/blocks/modules/blog-items/blog-items.js
@@ -0,0 +1,13 @@
+modules.define('blog-items', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
