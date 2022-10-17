bb3efbe27b07cdb9f7d36fb1a64e81c8ab6b651b
Егор Астрейко <e.astreiko@itrack.ru>
2022-10-17 15:28:14 +0300

    fix

--- html/src/blocks/modules/industry-specialization/industry-specialization.js
+++ html/src/blocks/modules/industry-specialization/industry-specialization.js
@@ -10,14 +10,17 @@ export default class IndustrySpecialization {
         const $forSlider = $for.find(".industry-specialization-slider-for__list");
 
         if($navSlider && $forSlider) {
-            $forSlider.slick({
+
+            const forSlider = $forSlider.slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 arrows: false,
                 asNavFor: $navSlider,
                 vertical: true,
-              });
-              $navSlider.slick({
+                swipe: false
+            });
+
+            const navSlider = $navSlider.slick({
                 slidesToShow: 5,
                 slidesToScroll: 1,
                 asNavFor: $forSlider,
@@ -27,6 +30,32 @@ export default class IndustrySpecialization {
                 prevArrow: $nav.find('.industry-specialization-slider-nav__btn--prev'),
                 nextArrow: $nav.find('.industry-specialization-slider-nav__btn--next')
               });
+
+            $(window).on("resize", function() {
+                if (window.innerWidth <= 1023) {
+                    $navSlider.slick('unslick');
+
+                    $('.industry-specialization__tabs-item:first').addClass('industry-specialization__tabs-item--active')
+
+                    $(document).on('click', '.industry-specialization__tabs-item', function() {
+                        $forSlider.slick('slickGoTo', $(this).index());
+                        $('.industry-specialization__tabs-item').removeClass('industry-specialization__tabs-item--active')
+                        $(this).addClass('industry-specialization__tabs-item--active')
+                    })
+                }
+                else {
+                    $navSlider.slick({
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        asNavFor: $navSlider,
+                        vertical: true,
+                        swipe: false
+                    });
+                }
+            });
+
+            $(window).trigger('resize');
         }
     }
 
