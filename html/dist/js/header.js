0d394e1a768fde524e7ca2b5456d1398586f3db6
Fayzulla Rahmatullayev <fayzullarakhmatullayev@gmail.com>
2022-12-26 14:02:55 +0500

    first commit

---
+++ html/dist/js/header.js
@@ -0,0 +1,280 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"header": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./src/blocks/modules/header/module.header.js","vendor"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/blocks/modules/header/module.header.js":
+/*!****************************************************!*\
+  !*** ./src/blocks/modules/header/module.header.js ***!
+  \****************************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _js_commonVue_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../../js/commonVue.js */ "./src/js/commonVue.js");
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.js");
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(vue__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var gsap__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! gsap */ "./node_modules/gsap/index.js");
+
+
+
+window.header = new vue__WEBPACK_IMPORTED_MODULE_1___default.a({
+  el: '#header',
+  data: function data() {
+    return {
+      isPhonesOpened: false,
+      isOpened: false,
+      header: null
+    };
+  },
+  mounted: function mounted() {
+    var _this = this;
+    window.addEventListener('resize', this.setHeaderWavePositionOnLoad);
+    setTimeout(function () {
+      _this.setHeaderWavePositionOnLoad();
+    }, 0);
+  },
+  destroyed: function destroyed() {
+    window.removeEventListener('resize', this.setHeaderWavePositionOnLoad);
+  },
+  methods: {
+    setHeaderWavePosition: function setHeaderWavePosition() {
+      gsap__WEBPACK_IMPORTED_MODULE_2__["default"].to('.header-menu-border', {
+        left: event.target.offsetLeft,
+        width: event.target.offsetWidth,
+        right: 'auto'
+      });
+    },
+    setHeaderWavePositionOnLoad: function setHeaderWavePositionOnLoad() {
+      if (document.querySelector('.header-menu li.isActive')) {
+        var active_menu = document.querySelector('.header-menu li.isActive');
+        gsap__WEBPACK_IMPORTED_MODULE_2__["default"].to('.header-menu-border', {
+          left: active_menu.offsetLeft,
+          width: active_menu.offsetWidth,
+          right: 'auto'
+        });
+        gsap__WEBPACK_IMPORTED_MODULE_2__["default"].to('.header-menu-wave', {
+          left: active_menu.offsetLeft + active_menu.offsetWidth / 2
+        });
+      }
+    },
+    openMobileHeader: function openMobileHeader() {
+      this.isOpened = !this.isOpened;
+      this.isPhonesOpened = false;
+      document.querySelector('html').classList.toggle('overflowed');
+    },
+    openPhonesModal: function openPhonesModal() {
+      this.isPhonesOpened = !this.isPhonesOpened;
+      this.isOpened = false;
+    },
+    closePhonesModal: function closePhonesModal() {
+      // if (window.innerWidth > 1023) {
+      this.isPhonesOpened = false;
+      document.querySelector('html').classList.remove('overflowed');
+      // }
+    },
+    onClickMobileMenuLink: function onClickMobileMenuLink(event) {
+      var modalMenu = event.target.closest('li').querySelector('.header-phone_modal');
+      var isModalMenu = Boolean(modalMenu);
+      if (isModalMenu) {
+        modalMenu.style.height = modalMenu.clientHeight === 0 ? "".concat(modalMenu.scrollHeight, "px") : 0;
+        event.preventDefault();
+      }
+    }
+  }
+});
+
+/***/ }),
+
+/***/ "./src/js/commonVue.js":
+/*!*****************************!*\
+  !*** ./src/js/commonVue.js ***!
+  \*****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.js");
+/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(vue__WEBPACK_IMPORTED_MODULE_0__);
+
+vue__WEBPACK_IMPORTED_MODULE_0___default.a.mixin({
+  data: function data() {
+    return {
+      window: {
+        width: window.innerWidth,
+        height: window.innerHeight
+      }
+    };
+  },
+  mounted: function mounted() {
+    var _this = this;
+    window.addEventListener('resize', function () {
+      _this.window = {
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    });
+  },
+  destroyed: function destroyed() {
+    window.addEventListener('resize', function () {});
+  }
+});
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=header.js.map
